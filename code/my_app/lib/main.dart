66029426f89fbc8067818fe63131d443e73bbebd
baurine <2008.hbl@gmail.com>
2019-04-06 14:35:11 +0800

    GestureDetector sample

--- code/my_app/lib/main.dart
+++ code/my_app/lib/main.dart
@@ -15,6 +15,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('my button is tapped');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500]
+        ),
+        child: Center(
+          child: Text('button'),
+        ),
+      ),
+    );
+  }
+}
+
 class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -35,7 +58,7 @@ class MyScaffold extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text('body'),
+        child: MyButton(),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
