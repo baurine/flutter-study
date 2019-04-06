6684b725861206743d3b33ff5d6f658421283405
baurine <2008.hbl@gmail.com>
2019-04-06 17:04:51 +0800

    Interactivity sample

--- code/my_app/lib/main.dart
+++ code/my_app/lib/main.dart
@@ -15,6 +15,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class MyScaffold extends StatelessWidget {
   Widget _buildTitleSection() => Container(
     padding: const EdgeInsets.all(32),
@@ -38,8 +83,7 @@ class MyScaffold extends StatelessWidget {
             ],
           ),
         ),
-        Icon(Icons.star, color: Colors.red[500]),
-        Text('41')
+        FavoriteWidget()
       ],
     ),
   );
