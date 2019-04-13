5fdf0fcc9977eb0ff887a2a77357fc36e93188e5
baurine <2008.hbl@gmail.com>
2019-04-13 20:51:01 +0800

    Step 4 - show the cute cats pictures from response

--- code/workshop_1/lib/main.dart
+++ code/workshop_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:workshop_1/model/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,19 +23,50 @@ class MyScaffold extends StatefulWidget {
 }
 
 class MyScaffoldState extends State<MyScaffold> {
+  User user;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('flutter workshop'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('you clicked', style: TextStyle(fontSize: 24),),
-          ],
-        ),
+      body: user == null ? 
+      _loading() :
+      _userContent(),
+    );
+  }
+
+  Widget _loading() {
+    return Center(
+      child: Text('loading...'),
+    );
+  }
+
+  Widget _userContent() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Text(user.data.name, style: TextStyle(color: Colors.black, fontSize: 24.0),),
+          SizedBox(height: 20.0,),
+          Text(user.data.email, style: TextStyle(color: Colors.black, fontSize: 15.0)),
+          SizedBox(height: 20.0,),
+          SizedBox(
+            height: 250.0 * user.data.pics.length,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              itemCount: user.data.pics.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Image(
+                  image: NetworkImage(user.data.pics[index]),
+                  width: 400.0,
+                  height: 250.0,
+                  fit: BoxFit.cover
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
@@ -48,8 +80,15 @@ class MyScaffoldState extends State<MyScaffold> {
 
   void loadData() async {
     try {
-      Response response = await Dio().get("http://www.mocky.io/v2/5cb1cdcf330000d81f5720ea");
-      print(response);
+      Response response = await Dio().get("http://www.mocky.io/v2/5cb1cdcf330000d81f5720ea?mocky-delay=2000ms");
+      // print(response);
+      // print(response.data);
+      // print(response.data['data']);
+      User _user = User.fromJson(response.data);
+      print(_user.data.email);
+      setState(() {
+        user = _user;
+      });
     } catch (e) {
       print(e);
     }
